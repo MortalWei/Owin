2c00229b1867fc3d6bf2f0f608b7ed286b9fab01
Mortal <mortal.wei@hotmail.com>
2019-08-29 10:58:59 +0800

    First

---
+++ src/OrdiOwin.Core/Settings/LoadedAssembliesSettings.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Configuration;
+
+namespace OrdiOwin.Core.Settings
+{    public class LoadedAssembliesSettings : ConfigurationSection    {        [ConfigurationProperty("", IsDefaultCollection = true)]        public AssemblyElementCollection AssemblyNames        {            get { return (AssemblyElementCollection)this[""]; }        }        public static LoadedAssembliesSettings GetSection()        {            return ConfigurationManager.GetSection("loadedAssemblies")                as LoadedAssembliesSettings;        }    }    public class AssemblyElementCollection : ConfigurationElementCollection    {        protected override ConfigurationElement CreateNewElement()        {            return new AssemblyElement();        }        protected override object GetElementKey(ConfigurationElement element)        {            AssemblyElement serviceTypeElement = (AssemblyElement)element;            return serviceTypeElement.AssemblyName;        }    }    public class AssemblyElement : ConfigurationElement    {        [ConfigurationProperty("assemblyName", IsRequired = true)]        public string AssemblyName        {            get { return (string)this["assemblyName"]; }            set { this["assemblyName"] = value; }        }    }
+}
