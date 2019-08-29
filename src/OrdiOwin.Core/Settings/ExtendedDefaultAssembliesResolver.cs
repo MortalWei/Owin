3ce7ef5e66454207588b6ac6b5aafce5b0e448ba
Mortal <mortal.wei@hotmail.com>
2019-08-29 14:41:44 +0800

    1

--- src/OrdiOwin.Core/Settings/ExtendedDefaultAssembliesResolver.cs
+++
@@ -1,10 +0,0 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Web.Http.Dispatcher;
-
-namespace OrdiOwin.Core.Settings
-{
-    public class ExtendedDefaultAssembliesResolver : DefaultAssembliesResolver    {        public override ICollection<Assembly> GetAssemblies()        {            LoadedAssembliesSettings settings = LoadedAssembliesSettings.GetSection();            if (null != settings)            {                foreach (AssemblyElement element in settings.AssemblyNames)                {                    AssemblyName assemblyName = AssemblyName.GetAssemblyName(element.AssemblyName);                    if (!AppDomain.CurrentDomain.GetAssemblies().Any(assembly => AssemblyName.ReferenceMatchesDefinition(assembly.GetName(), assemblyName)))                    {                        AppDomain.CurrentDomain.Load(assemblyName);                    }                }            }            return base.GetAssemblies();        }    }
-}
